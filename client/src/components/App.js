facf660e387bb5ffe57e933e1c5f817d1b3408fd
ajay-2019011 <72451943+ajay-2019011@users.noreply.github.com>
2022-10-11 03:09:48 +0530

    Making components folder to store components

---
+++ client/src/components/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import '../styles/App.css';
+import '../index.css'
+import Navbar from './Navbar'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Navbar />
+      </header>
+      <footer></footer>
+    </div>
+  );
+}
+
+export default App;
