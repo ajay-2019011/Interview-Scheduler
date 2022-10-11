1929ad797caf37b1998a2c7d616377257f69be0c
ajay-2019011 <72451943+ajay-2019011@users.noreply.github.com>
2022-10-12 04:29:14 +0530

    Made some minor changes for design

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -1,38 +1,73 @@
 import React from "react"
-// import DatePicker from 'react-date-picker'
+import DatePicker from 'react-date-picker'
+import { TimePicker } from 'react-ios-time-picker';
 import '../styles/Form.css'
 
 export default function Form() {
-    const [formData, setFormData] = React.useState(
-        {
-            name: "", 
-            email: "", 
-            mobile: "",
-            college: "", 
-            position: "",
-            date: "",
-            toSend: false,
-            comments: ""
-        }
-    )
-    
-    function handleChange(event) {
-        const {name, value, type, checked} = event.target
-        console.log(event.target)
-        setFormData(prevFormData => {
-            return {
-                ...prevFormData,
-                [name]: type === "checkbox" ? checked : value
-            }
-        })
+    const [name, setName] = React.useState("");
+    const [email, setEmail] = React.useState("");
+    const [mobile, setMobile] = React.useState("");
+    const [college, setCollege] = React.useState("");
+    const [position, setPosition] = React.useState("");
+    const [date, setDate] = React.useState(new Date());
+    const [startTime, setStartTime] = React.useState("14:04");
+    const [endTime, setEndTime] = React.useState("17:23");
+    const [toSend, setToSend] = React.useState(false);
+    // const [formData, setFormData] = React.useState(
+    //     {
+    //         name: "", 
+    //         email: "", 
+    //         mobile: "",
+    //         college: "", 
+    //         position: "",
+    //         date: "",
+    //         toSend: false
+    //     }
+    // )
+    // function handleChange(event) {
+    //     const {name, value, type, checked} = event.target
+    //     console.log(event.target)
+    //     setFormData(prevFormData => {
+    //         return {
+    //             ...prevFormData,
+    //             [name]: type === "checkbox" ? checked : value
+    //         }
+    //     })
+    // }
+    function handleNameChange(event){
+        setName(event.target.value)
+    }
+    function handleEmailChange(event){
+        setEmail(event.target.value)
+    }
+    function handleMobileChange(event){
+        setMobile(event.target.value)
+    }
+    function handleCollegeChange(event){
+        setCollege(event.target.value)
+    }
+    function handlePositionChange(event){
+        setPosition(event.target.value)
+    }
+    function handleSendChange(event){
+        setToSend(event.target.checked)
+    }
+    function handleDateChange(event){
+        console.log(JSON.stringify(event))
+        setDate(new Date(event))
+    }
+    function handleStartTimeChange(event){
+        setStartTime(event.value)
+    }
+    function handleEndTimeChange(event){
+        setEndTime(event.value)
     }
     
     function handleSubmit(event) {
         event.preventDefault()
         // submitToApi(formData)
-        console.log(formData)
+        console.log(name, email, mobile, college, position, toSend, date, startTime)
     }
-    
     return (
         <div className="form-wrapper">
             <h1 id="form-heading">Fill the given Form</h1>
@@ -45,9 +80,9 @@ export default function Form() {
                     <input
                         type="text"
                         placeholder="Candidate Name"
-                        onChange={handleChange}
+                        onChange={handleNameChange}
                         name="name"
-                        value={formData.name}
+                        value={name}
                     />
                 </div>
                 <div>
@@ -55,9 +90,9 @@ export default function Form() {
                     <input
                         type="email"
                         placeholder="Email"
-                        onChange={handleChange}
+                        onChange={handleEmailChange}
                         name="email"
-                        value={formData.email}
+                        value={email}
                     />
                 </div>
                 <div>
@@ -65,9 +100,9 @@ export default function Form() {
                     <input
                         type="text"
                         placeholder="98456XXXXX"
-                        onChange={handleChange}
+                        onChange={handleMobileChange}
                         name="mobile"
-                        value={formData.mobile}
+                        value={mobile}
                     />
                 </div>
                 <div>
@@ -75,9 +110,9 @@ export default function Form() {
                     <input
                         type="text"
                         placeholder="College Name"
-                        onChange={handleChange}
+                        onChange={handleCollegeChange}
                         name="college"
-                        value={formData.college}
+                        value={college}
                     />
                 </div>
                 <div>
@@ -85,8 +120,8 @@ export default function Form() {
                     <br />
                     <select 
                         id="position" 
-                        value={formData.position}
-                        onChange={handleChange}
+                        value={position}
+                        onChange={handlePositionChange}
                         name="position"
                     >
                         <option value="">--Available Positions--</option>
@@ -100,33 +135,40 @@ export default function Form() {
                         <option value="hr">Human Resources</option>
                     </select>
                 </div>
-                {/* <div>
+                <div>
                     <label htmlFor="date">Date for Interview :  </label>
                     <DatePicker 
                         id="date"
                         name="date"
-                        value={formData.date}
-                        onChange={handleChange} 
+                        value={date}
+                        onChange={handleDateChange} 
                     />
-                </div> */}
+                </div>
+                <div>
+                    <label htmlFor="startTime">Start time for Interview :  </label>
+                    <TimePicker 
+                        id="startTime"
+                        name="startTime"
+                        onChange={handleStartTimeChange} 
+                        value={startTime} 
+                    />
+                    <label htmlFor="endTime">End time for Interview :  </label>
+                    <TimePicker 
+                        id="endTime"
+                        name="endTime"
+                        onChange={handleEndTimeChange} 
+                        value={endTime} 
+                    />
+                </div>
                 <div id="checkbox">
                     <input 
                         type="checkbox" 
                         id="toSend" 
-                        checked={formData.toSend}
-                        onChange={handleChange}
+                        checked={toSend}
+                        onChange={handleSendChange}
                         name="toSend"
                     />
-                    <label htmlFor="toSend">  Do you want to notify the candidate by E-mail?</label>
-                </div>
-                <div>
-                    <label htmlFor="comments">Comments:  </label>
-                    <textarea 
-                        value={formData.comments}
-                        placeholder="Any Comments"
-                        onChange={handleChange}
-                        name="comments"
-                    />
+                    <label htmlFor="toSend">Do you want to notify the candidate by E-mail?</label>
                 </div>
                 <button id="submit">Submit</button>
         </form>
